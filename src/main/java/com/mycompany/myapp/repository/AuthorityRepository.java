2d9faacc873bb5286350d670ab87510fcb9878db
Juha Nissinen <juha.nissinen@tieto.com>
2016-09-08 19:39:33 +0300

    third commit

--- src/main/java/com/mycompany/myapp/repository/AuthorityRepository.java
+++ src/main/java/com/mycompany/myapp/repository/AuthorityRepository.java
@@ -5,7 +5,7 @@ import com.mycompany.myapp.domain.Authority;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * Spring Data JPA repository for the Authority entity, jabadabaduu.
+ * Spring Data JPA repository for the Authority entity
  */
 public interface AuthorityRepository extends JpaRepository<Authority, String> {  
 }
